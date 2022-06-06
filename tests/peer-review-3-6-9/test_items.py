8f5604205b268da733a192a8d6a9493ff7f7e998
anon-guthub-user <106264523+anon-guthub-user@users.noreply.github.com>
2022-06-06 15:54:51 +0300

    Minor refactoring:
    - Delete comments that are now useless;
    
    - Rename stuff so that the names are more intuitive.

--- tests/peer-review-3-6-9/test_items.py
+++ tests/peer-review-3-6-9/test_items.py
@@ -1,11 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
-def test_cart_visibility(browser):
+def test_cart_button_visibility(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
 
-    # User find_elements to find the cart so that it returns an array of elements.
-    cart = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+    cart_button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
 
-    # Assert that the list is not empty.
-    assert cart.is_displayed(), "Cart element is not present on the page."
+    assert cart_button.is_displayed(), "Cart element is not present on the page."
