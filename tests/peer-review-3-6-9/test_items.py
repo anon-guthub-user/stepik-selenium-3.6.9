af1bc8f04353b4fcafc6b7af698610031c9a1476
anon-guthub-user <106264523+anon-guthub-user@users.noreply.github.com>
2022-06-06 15:22:08 +0300

    Change test behaviour:
    - Check for element visibility instead of existence.

--- tests/peer-review-3-6-9/test_items.py
+++ tests/peer-review-3-6-9/test_items.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
-def test_cart_existence(browser):
+def test_cart_visibility(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
 
     # User find_elements to find the cart so that it returns an array of elements.
-    cart = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
+    cart = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
 
     # Assert that the list is not empty.
-    assert cart, "Cart element is not present on the page."
+    assert cart.is_displayed(), "Cart element is not present on the page."
